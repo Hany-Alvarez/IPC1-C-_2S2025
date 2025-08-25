b47b6c56ecb54f99009f227f099d42ccf82588ed
HanyBulux <hanyalvarezbuluxdiploma@gmail.com>
2025-08-24 20:57:51 -0600

    Agregando Librerias
    Commit 2 de semana 1

--- Proyecto 1/Sistema_de_inventario_tienda_ropa/src/main/java/com/mycompany/sistema_de_inventario_tienda_ropa/Sistema_de_inventario_tienda_ropa.java
+++ Proyecto 1/Sistema_de_inventario_tienda_ropa/src/main/java/com/mycompany/sistema_de_inventario_tienda_ropa/Sistema_de_inventario_tienda_ropa.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.sistema_de_inventario_tienda_ropa;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  *
  * @author hanya
