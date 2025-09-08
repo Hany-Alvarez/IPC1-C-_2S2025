2d50968ec28c118f9e4f64d0a8adfd1edd3ad5d3
HanyBulux <hanyalvarezbuluxdiploma@gmail.com>
2025-09-08 02:01:53 -0600

    Creando métodos

---
+++ Proyecto 1/Sistema_de_inventario_tienda_ropa/src/main/java/com/mycompany/sistema_de_inventario_tienda_ropa/Métodos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistema_de_inventario_tienda_ropa;
+
+/**
+ *
+ * @author hanya
+ */
+public class Métodos {
+    
+}
--- Proyecto 1/Sistema_de_inventario_tienda_ropa/src/main/java/com/mycompany/sistema_de_inventario_tienda_ropa/Sistema_de_inventario_tienda_ropa.java
+++ Proyecto 1/Sistema_de_inventario_tienda_ropa/src/main/java/com/mycompany/sistema_de_inventario_tienda_ropa/Sistema_de_inventario_tienda_ropa.java
@@ -1,40 +1,41 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.sistema_de_inventario_tienda_ropa;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 /**
  *
  * @author hanya
  */
 public class Sistema_de_inventario_tienda_ropa {
-    
-    public static void main(String[] args) {
-     InputStreamReader capturarTeclado = new InputStreamReader(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //Declarar variables
+        String[] Persona = new String[101];//Arreglo para el registro de personas que usan el programa
+
+        //Entrada de texto
+        InputStreamReader capturarTeclado = new InputStreamReader(System.in);
         BufferedReader buffer = new BufferedReader(capturarTeclado);
 
-        System.out.println("¡Bienvenido elige una opción!");
-        String N="Natalia";
-        int A=2;
-        mensaje_de_venta(N,A);
         boolean salir = false;
+
+        do {
+
+            System.out.println("¡Bienvenido a la Tienda de Ropa Jujutsu Kaisen!");
+            System.out.println("");
+            System.out.println("Escribe tu nombre para acceder al inventario ");
+            String nombre=buffer.readLine();
+            //Guardar el nombre en el arreglo "Persona"
+            
+            
         
-         do {
-             
-         }while (!salir);
-        
-        
-        
-        
- 
+
+        } while (!salir);
+
     }
     
-    static void mensaje_de_venta(String nombre,int veces){
-        System.out.println("Hola "+nombre+" esta es tu intento "+veces);
- 
-    }
 }
