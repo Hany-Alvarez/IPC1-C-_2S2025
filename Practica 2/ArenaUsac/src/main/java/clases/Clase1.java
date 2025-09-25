9add028fcae5c9fec2a6e733d09c0980700055db
HanyBulux <hanyalvarezbuluxdiploma@gmail.com>
2025-09-24 21:18:31 -0600

    df

--- Practica 2/ArenaUsac/src/main/java/clases/Clase1.java
+++ Practica 2/ArenaUsac/src/main/java/clases/Clase1.java
@@ -3,11 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package clases;
+import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+import java.awt.*;
 
-/**
- *
- * @author hanya
- */
-public class Clase1 {
+public class Clase1 extends JFrame{// Se hace una clase hija de JFrame
+    
+private final JButton btnAbrir=new JButton("Abrir arena");
+
+//
+//
+// Constructor de la Clase Clase1
+    public Clase1(String title) throws HeadlessException {// Es para manejar las excepciones de que el sistema operativo no tenga interfaz gráfica o no sea compatible
+        super(title);
+        Dimension d=new Dimension (500,500);//Se crea una variable para dimensionar en px
+        this.setSize(d);//Esto es para dimensionar la ventana
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Para cerrar a la ventana
+        this.setResizable(false);//Para no permitir modificar el tamañan de la ventana
+       
+    
+    }
+
+
     
 }
+
--- Practica 2/ArenaUsac/src/main/java/com/mycompany/arenausac/ArenaUsac.java
+++ Practica 2/ArenaUsac/src/main/java/com/mycompany/arenausac/ArenaUsac.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.arenausac;
 
+import clases.Clase1;
+
 /**
  *
  * @author hanya
@@ -11,20 +13,12 @@ package com.mycompany.arenausac;
 public class ArenaUsac {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+     Clase1 C=new Clase1("Arena de Peleas Pokemón_"+"_PID de la ventanta: "+getPID());
+     C.setVisible(true);
+    
+    }
+    
+    public static String getPID(){//Para ver si  realmente la ventana se cerro y no solo se oculto
+        return java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
     }
 }
